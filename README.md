0fb391a1ccd26774878a317d5504bc67167593bd
Marcin Gryszkalis <mg@fork.pl>
2017-09-05 21:38:21 +0200

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+use: quiz.html#abc
+to load from config from abc.json
---
+++ qwtime.js
@@ -0,0 +1,191 @@
+
+var cfg
+var cfgid
+var qs // question shuffled
+var state = "wait"; // "wait", "run", "finish"
+
+var nextmoment
+
+var starttime
+var lasttime
+var lastdiff
+var round
+var fmain_fontsize
+
+function updateClock() 
+{
+    if (state == "wait") 
+    { 
+        $("#ftime_val").html("00:00.0")
+    }
+    else if (state == "run")
+    {
+        lasttime = moment()
+        var tdiff = moment(lasttime).diff(starttime)
+        lastdiff = moment(tdiff).format('mm:ss.S')
+        $("#ftime_val").html(lastdiff)
+    }
+    else // finish
+    {
+
+    }
+}
+
+// https://github.com/Daplie/knuth-shuffle/blob/master/index.js
+function shuffle(array) {
+var currentIndex = array.length
+  , temporaryValue
+  , randomIndex
+  ;
+
+// While there remain elements to shuffle...
+while (0 !== currentIndex) {
+
+  // Pick a remaining element...
+  randomIndex = Math.floor(Math.random() * currentIndex);
+  currentIndex -= 1;
+
+  // And swap it with the current element.
+  temporaryValue = array[currentIndex];
+  array[currentIndex] = array[randomIndex];
+  array[randomIndex] = temporaryValue;
+}
+
+return array;
+}
+
+function prepareClock()
+{
+
+}
+
+function click_next() 
+{
+    if (state == "wait")
+    {
+        state = "run"
+        $("#btn_next_label").html("Dalej")
+        round = 1;
+        starttime = moment()
+    }
+    else if (state == "run")
+    {
+        round++
+        if (round == cfg.rounds) // last round
+        {
+            $("#btn_next_label").html("Koniec")
+        }
+        else if (round > cfg.rounds)
+        {
+            state = "finish"
+            $("#fmain").css("font-size", fmain_fontsize)
+            $("#fmain").html("KONIEC")
+            $("#ftime_val").css("visibility", "visible")
+            $("#btn_next_label").html("Od nowa")           
+        }
+    }
+    else if (state == "finish") // start again
+    {
+        state = "wait"
+        qs = shuffle(qs) // reshuffle
+        $("#btn_next_label").html("Start")
+        $("#fmain").html("")
+        $("#fmain").css("font-size", cfg.fontsize + "vh")
+    }
+
+    if (state == "run")
+    {
+        var q = qs[round - 1]
+        if (q.match(/\.(png|jpe?g)/))
+        {
+            $("#fmain").html("<img src='"+cfgid.substr(1)+"/"+q+"'>")
+        }   
+        else
+        {
+            $("#fmain").html(q)
+        }
+    }
+}
+
+function click_prev()
+{
+    if (state == "wait")
+    {
+        // nothing
+    }
+    else if (state == "run" || state == "finish")
+    {
+        if (round > 1)
+        {
+            round -= 2
+            $("#btn_next_label").html("Dalej")
+            $("#fmain").css("font-size", cfg.fontsize + "vh")
+            state = "run"
+            click_next();
+        }
+    }
+
+}
+
+function click_time()
+{
+    if ($("#ftime_val").css("visibility") == "visible")
+    {
+        $("#ftime_val").css("visibility", "hidden")
+    }
+    else
+    {
+        $("#ftime_val").css("visibility", "visible")
+    }
+
+}
+
+$( window ).resize(function() 
+{
+    // prepareClock();
+});
+
+$( document ).ready(function() 
+{
+    moment.locale("pl");
+
+    $.ajaxSetup({
+        async: false
+    });
+
+    cfgid = "#x10";
+    if (window.location.hash)
+    {
+        cfgid = window.location.hash;
+    }
+
+    var cfgjson = cfgid.substr(1) + ".json"
+
+    $.getJSON(cfgjson)
+    .done(function(json) 
+    { 
+        cfg = json; 
+    })
+    .fail(function( jqxhr, textStatus, error) 
+    {
+        var err = textStatus + ", " + error;
+        alert("JSON ERROR in file " + cfgjson + ": " + err );
+    });
+
+    // $("#ftitle").html(cfg.title + " " + cfg.rounds + "/" + cfg.questions.length + ", czas: " + cfg.timelimit + "s");
+    $("#ftitle").html(cfg.title + ", czas: " + cfg.timelimit + "s");
+
+    fmain_fontsize = $("#fmain").css("font-size")
+    $("#fmain").css("font-size", cfg.fontsize + "vh")
+    qs = cfg.questions;
+    qs = shuffle(qs);
+
+
+    // prepareHours();
+    // prepareClock();
+    
+    setInterval(updateClock, 100);
+
+
+});
+
