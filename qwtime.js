860a627857185c02edc6e9baeca52dbb93cc4895
Marcin Gryszkalis <mg@fork.pl>
2023-05-24 11:13:55 +0200

    cleanup

--- qwtime.js
+++ qwtime.js
@@ -2,7 +2,7 @@ var cfg
 var cfgid
 var qs // questions shuffled
 var as // answers (possibly undefined)
-var state = "wait"; // "wait", "run", "finish"
+var state = "wait" // "wait", "run", "finish"
 
 var nextmoment
 
@@ -34,33 +34,33 @@ function updateClock()
 
 // https://github.com/Daplie/knuth-shuffle/blob/master/index.js
 function shuffle(array, arraz) {
-var currentIndex = array.length;
-var temporaryValue;
-var randomIndex;
+var currentIndex = array.length
+var temporaryValue
+var randomIndex
 
 // While there remain elements to shuffle...
 while (0 !== currentIndex) {
 
   // Pick a remaining element...
-  randomIndex = Math.floor(Math.random() * currentIndex);
-  currentIndex -= 1;
+  randomIndex = Math.floor(Math.random() * currentIndex)
+  currentIndex -= 1
 
   // And swap it with the current element.
-  temporaryValue = array[currentIndex];
-  array[currentIndex] = array[randomIndex];
-  array[randomIndex] = temporaryValue;
+  temporaryValue = array[currentIndex]
+  array[currentIndex] = array[randomIndex]
+  array[randomIndex] = temporaryValue
 
   if (arraz)
     {
-   temporaryValue = arraz[currentIndex];
-  arraz[currentIndex] = arraz[randomIndex];
-  arraz[randomIndex] = temporaryValue;
+   temporaryValue = arraz[currentIndex]
+  arraz[currentIndex] = arraz[randomIndex]
+  arraz[randomIndex] = temporaryValue
 
        
     }
 }
 
-// return array;
+// return array
 }
 
 function click_next() 
@@ -70,7 +70,7 @@ function click_next()
     {
         state = "run"
         $("#btn_next_label").html("Dalej")
-        round = 1;
+        round = 1
         starttime = moment()
     }
     else if (state == "run")
@@ -135,7 +135,7 @@ function click_prev()
             $("#btn_next_label").html("Dalej")
             $("#fmain_val").css("font-size", cfg.fontsize + "vh")
             state = "run"
-            click_next();
+            click_next()
         }
     }
 
@@ -195,21 +195,21 @@ function click_time()
 
 $( window ).resize(function() 
 {
-    // prepareClock();
-});
+    // prepareClock()
+})
 
 $( document ).ready(function() 
 {
-    moment.locale("pl");
+    moment.locale("pl")
 
     $.ajaxSetup({
         async: false
-    });
+    })
 
-    cfgid = "#a10";
+    cfgid = "#a10"
     if (window.location.hash)
     {
-        cfgid = window.location.hash;
+        cfgid = window.location.hash
     }
 
     var cfgjson = cfgid.substr(1) + ".json"
@@ -217,48 +217,48 @@ $( document ).ready(function()
     $.getJSON(cfgjson)
     .done(function(json) 
     { 
-        cfg = json; 
+        cfg = json 
     })
     .fail(function( jqxhr, textStatus, error) 
     {
-        var err = textStatus + ", " + error;
-        alert("JSON ERROR in file " + cfgjson + ": " + err );
-    });
+        var err = textStatus + ", " + error
+        alert("JSON ERROR in file " + cfgjson + ": " + err )
+    })
 
-    // $("#ftitle").html(cfg.title + " " + cfg.rounds + "/" + cfg.questions.length + ", czas: " + cfg.timelimit + "s");
-    $("#ftitle").html(cfg.title + ", czas: " + cfg.timelimit);
+    // $("#ftitle").html(cfg.title + " " + cfg.rounds + "/" + cfg.questions.length + ", czas: " + cfg.timelimit + "s")
+    $("#ftitle").html(cfg.title + ", czas: " + cfg.timelimit)
 
     fmain_fontsize = $("#fmain_val").css("font-size")
     $("#fmain_val").css("font-size", cfg.fontsize + "vh")
-    qs = cfg.questions;
-    as = cfg.answers;
+    qs = cfg.questions
+    as = cfg.answers
 
     if (as && qs.length != as.length)
     {
-        alert("ERROR in file " + cfgjson + ": number of questions is different from number of answers");
+        alert("ERROR in file " + cfgjson + ": number of questions is different from number of answers")
     }
 
-    shuffle(qs, as);
+    shuffle(qs, as)
 
     if (as)
     {
-        document.getElementById("btn_check").style.visibility = "visible";
-        document.getElementById("btn_check").style.display = "block";
-        document.getElementById("btn_next").style.width = "calc(24.5% - 1vh)";
-        document.getElementById("btn_prev").style.width = "calc(24.5% - 1vh)";
-        document.getElementById("btn_reset").style.width = "calc(24.5% - 1vh)";
-        document.getElementById("btn_check").style.width = "calc(24.5% - 1vh)";
+        $("#btn_check").css("visibility", "visible")
+        $("#btn_check").css("display", "block")
+        $("#btn_next").css("width", "calc(24.5% - 1vh)")
+        $("#btn_prev").css("width", "calc(24.5% - 1vh)")
+        $("#btn_reset").css("width", "calc(24.5% - 1vh)")
+        $("#btn_check").css("width", "calc(24.5% - 1vh)")
     }
     else
     {
-        document.getElementById("btn_check").style.visibility = "hidden";
-        document.getElementById("btn_check").style.display = "none";
-        document.getElementById("btn_next").style.width = "calc(33% - 1vh)";
-        document.getElementById("btn_prev").style.width = "calc(33% - 1vh)";
-        document.getElementById("btn_reset").style.width = "calc(33% - 1vh)";
+        $("#btn_check").css("visibility", "hidden")
+        $("#btn_check").css("display", "none")
+        $("#btn_next").css("width", "calc(33% - 1vh)")
+        $("#btn_prev").css("width", "calc(33% - 1vh)")
+        $("#btn_reset").css("width", "calc(33% - 1vh)")
     }
 
-    setInterval(updateClock, 100);
+    setInterval(updateClock, 100)
 
-});
+})
 
